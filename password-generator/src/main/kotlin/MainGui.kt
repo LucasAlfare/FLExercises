5232928201ffed9bc93c1e3c0aad01782f4a3d58
LucasAlfare <god-lucas@hotmail.com>
2024-01-19 01:03:51 -0300

    fix #3

--- password-generator/src/main/kotlin/MainGui.kt
+++ password-generator/src/main/kotlin/MainGui.kt
@@ -3,10 +3,10 @@
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,6 +22,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import ui.theme.ExercisesTheme
 
 private val sliderRange = 3f..50f
 
@@ -32,113 +33,115 @@ fun main() = application {
     ),
     onCloseRequest = { exitApplication() }
   ) {
-    var includeNumbers by remember { mutableStateOf(true) }
-    var includeLetters by remember { mutableStateOf(true) }
-    var includeSpecialCharacters by remember { mutableStateOf(true) }
-    var size by remember { mutableStateOf(15) }
-    var result by remember { mutableStateOf(PasswordGenerator.getRandomPassword()) }
-    var sliderValue by remember { mutableStateOf(15f) }
+    ExercisesTheme {
+      var includeNumbers by remember { mutableStateOf(true) }
+      var includeLetters by remember { mutableStateOf(true) }
+      var includeSpecialCharacters by remember { mutableStateOf(true) }
+      var size by remember { mutableStateOf(15) }
+      var result by remember { mutableStateOf(PasswordGenerator.getRandomPassword()) }
+      var sliderValue by remember { mutableStateOf(15f) }
 
-    Column(
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center,
-      modifier = Modifier.fillMaxSize()
-    ) {
       Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.weight(0.8f).fillMaxWidth().padding(8.dp)
+        modifier = Modifier.fillMaxSize()
       ) {
-        Box(
-          modifier = Modifier
-            .border(width = 2.dp, color = MaterialTheme.colors.primary, shape = RoundedCornerShape(12))
-            .fillMaxWidth()
+        Column(
+          horizontalAlignment = Alignment.CenterHorizontally,
+          verticalArrangement = Arrangement.Center,
+          modifier = Modifier.weight(0.8f).fillMaxWidth().padding(8.dp)
         ) {
-          Text(
-            text = result,
-            fontSize = 24.sp,
-            fontFamily = FontFamily.Monospace,
-            modifier = Modifier.align(Alignment.Center).padding(8.dp)
-          )
-        }
-
-        IconButton(
-          onClick = {
-            // TODO: pout result in clipboard
-          },
-          modifier = Modifier
-            .align(Alignment.End)
-            .padding(8.dp)
-            .size(16.dp)
-        ) {
-          Icon(
-            imageVector = Icons.Filled.Check,
-            contentDescription = "",
-            tint = MaterialTheme.colors.primary.copy(alpha = 0.4f)
-          )
-        }
-      }
-
-      Box(modifier = Modifier.weight(0.2f)) {
-        IconButton(onClick = {
-          result = PasswordGenerator.getRandomPassword(
-            size = size,
-            includeNumbers = includeNumbers,
-            includeLetters = includeLetters,
-            includeSpecialCharacters = includeSpecialCharacters
-          )
-        }) {
-          Icon(
-            imageVector = Icons.Filled.Refresh,
-            contentDescription = "",
-            modifier = Modifier.size(24.dp)
-          )
-        }
-      }
-
-      Column(modifier = Modifier.weight(1f).padding(8.dp).fillMaxWidth()) {
-        Column {
-          Text(text = "password size: ${sliderValue.toInt()}")
-          Row(verticalAlignment = Alignment.CenterVertically) {
+          Box(
+            modifier = Modifier
+              .border(width = 2.dp, color = MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(12))
+              .fillMaxWidth()
+          ) {
             Text(
-              text = sliderRange.start.toInt().toString(),
-              textAlign = TextAlign.Center,
-              modifier = Modifier.weight(0.1f)
+              text = result,
+              fontSize = 24.sp,
+              fontFamily = FontFamily.Monospace,
+              modifier = Modifier.align(Alignment.Center).padding(8.dp)
             )
-            Slider(
-              value = sliderValue,
-              valueRange = sliderRange,
-              onValueChange = { sliderValue = it },
-              onValueChangeFinished = { size = sliderValue.toInt() },
-              modifier = Modifier.weight(1f)
-            )
-            Text(
-              text = sliderRange.endInclusive.toInt().toString(),
-              textAlign = TextAlign.Center,
-              modifier = Modifier.weight(0.1f)
+          }
+
+          IconButton(
+            onClick = {
+              // TODO: pout result in clipboard
+            },
+            modifier = Modifier
+              .align(Alignment.End)
+              .padding(8.dp)
+              .size(16.dp)
+          ) {
+            Icon(
+              imageVector = Icons.Filled.Check,
+              contentDescription = "",
+              tint = MaterialTheme.colorScheme.primary.copy(alpha = 0.4f)
             )
           }
         }
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
-          Checkbox(checked = includeNumbers, onCheckedChange = {
-            includeNumbers = it
-          })
-          Text("Include numbers")
+        Box(modifier = Modifier.weight(0.2f)) {
+          IconButton(onClick = {
+            result = PasswordGenerator.getRandomPassword(
+              size = size,
+              includeNumbers = includeNumbers,
+              includeLetters = includeLetters,
+              includeSpecialCharacters = includeSpecialCharacters
+            )
+          }) {
+            Icon(
+              imageVector = Icons.Filled.Refresh,
+              contentDescription = "",
+              modifier = Modifier.size(24.dp)
+            )
+          }
         }
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
-          Checkbox(checked = includeLetters, onCheckedChange = {
-            includeLetters = it
-          })
-          Text("Include letters")
-        }
+        Column(modifier = Modifier.weight(1f).padding(8.dp).fillMaxWidth()) {
+          Column {
+            Text(text = "password size: ${sliderValue.toInt()}")
+            Row(verticalAlignment = Alignment.CenterVertically) {
+              Text(
+                text = sliderRange.start.toInt().toString(),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.weight(0.1f)
+              )
+              Slider(
+                value = sliderValue,
+                valueRange = sliderRange,
+                onValueChange = { sliderValue = it },
+                onValueChangeFinished = { size = sliderValue.toInt() },
+                modifier = Modifier.weight(1f)
+              )
+              Text(
+                text = sliderRange.endInclusive.toInt().toString(),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.weight(0.1f)
+              )
+            }
+          }
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
-          Checkbox(checked = includeSpecialCharacters, onCheckedChange = {
-            includeSpecialCharacters = it
-          })
-          Text("Include special characters")
+          Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(checked = includeNumbers, onCheckedChange = {
+              includeNumbers = it
+            })
+            Text("Include numbers")
+          }
+
+          Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(checked = includeLetters, onCheckedChange = {
+              includeLetters = it
+            })
+            Text("Include letters")
+          }
+
+          Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(checked = includeSpecialCharacters, onCheckedChange = {
+              includeSpecialCharacters = it
+            })
+            Text("Include special characters")
+          }
         }
       }
     }
